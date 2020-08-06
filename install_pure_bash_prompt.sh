2c47629d41dbf4c8ef6ec31a96c0b64b2c1ca222
ByNoMeans <62671086+ByNoMeans@users.noreply.github.com>
2020-08-05 22:26:22 -0500

    Update install_pure_bash_prompt.sh

--- install_pure_bash_prompt.sh
+++ install_pure_bash_prompt.sh
@@ -1,13 +1,16 @@
-# Copy bash.bashrc or bash-documented.bashrc to /Git/etc/. If you prefer documentation and chose the latter, make sure to rename it to bash.bashrc
-cp -i ~/.pure_bash_prompt/bash.bashrc /c/Program\ Files/Git/etc
 
-# Copy .minttyrc, .bash_profile, .git-completion.bash, .bash_aliases, and .bashrc to ~
-cp -i ~/.pure_bash_prompt/.minttyrc ~ && cp -i ~/.pure_bash_prompt/.bash_profile ~ && cp -i ~/.pure_bash_prompt/.git-completion.bash ~ && cp -i ~/.pure_bash_prompt/.bash_aliases ~ && cp -i ~/.pure_bash_prompt/.bashrc ~
+if [ -f ~/.pure_bash_prompt ]; then
+  # Copy bash.bashrc or bash-documented.bashrc to /Git/etc/. If you prefer documentation and chose the latter, make sure to rename it to bash.bashrc
+  cp -i ~/.pure_bash_prompt/bash.bashrc /c/Program\ Files/Git/etc
 
-# Install gistatus to immensely speed up the rate the prompt appears. It is
-# sourced within .bash_profile at the beginning of each session to be used, so
-# you don't need to run the second command of the repo's instructions. Just run
-git clone --depth=1 https://github.com/romkatv/gitstatus.git ~/gitstatus
+  # Copy .minttyrc, .bash_profile, .git-completion.bash, .bash_aliases, and .bashrc to ~
+  cp -i ~/.pure_bash_prompt/.minttyrc ~ && cp -i ~/.pure_bash_prompt/.bash_profile ~ && cp -i ~/.pure_bash_prompt/.git-completion.bash ~ && cp -i ~/.pure_bash_prompt/.bash_aliases ~ && cp -i ~/.pure_bash_prompt/.bashrc ~
 
-# Don't let gitstatus be the default prompt
-sed -i "$(($(wc -l <~/gitstatus/gitstatus.prompt.sh) - 20)),\$d" ~/gitstatus/gitstatus.prompt.sh
+  # Install gistatus to immensely speed up the rate the prompt appears. 
+  git clone --depth=1 https://github.com/romkatv/gitstatus.git ~/gitstatus
+
+  # Don't let gitstatus be the default prompt
+  sed -i "$(($(wc -l <~/gitstatus/gitstatus.prompt.sh) - 20)),\$d" ~/gitstatus/gitstatus.prompt.sh
+else 
+  echo "Install gitstatus before installing pure-bash-prompt"
+fi
