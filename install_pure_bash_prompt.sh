6814c4979a0e92ffe3fa60f98607671ec4e33db8
ByNoMeans <62671086+ByNoMeans@users.noreply.github.com>
2020-08-06 00:20:18 -0500

    Update install_pure_bash_prompt.sh

--- install_pure_bash_prompt.sh
+++ install_pure_bash_prompt.sh
@@ -1,18 +1,17 @@
-
-if [ -f ~/.pure_bash_prompt ]; then
-  if [ -f ~/gitstatus ]; then 
-    if [ -f /c/Program\ Files/Git/ ]; then
+if [ -e ~/.pure_bash_prompt ]; then
+  if [ -e ~/gitstatus ]; then
+    if [ -e /c/Program\ Files/Git/ ]; then
       # Copy bash.bashrc or bash-documented.bashrc to /Git/etc/. If you prefer documentation and chose the latter, make sure to rename it to bash.bashrc
       cp -i ~/.pure_bash_prompt/bash.bashrc /c/Program\ Files/Git/etc
 
       # Copy .minttyrc, .bash_profile, .git-completion.bash, .bash_aliases, and .bashrc to ~
       cp -i ~/.pure_bash_prompt/.minttyrc ~
-      cp -i ~/.pure_bash_prompt/.bash_profile ~ 
-      cp -i ~/.pure_bash_prompt/.git-completion.bash ~ 
-      cp -i ~/.pure_bash_prompt/.bash_aliases ~ 
+      cp -i ~/.pure_bash_prompt/.bash_profile ~
+      cp -i ~/.pure_bash_prompt/.git-completion.bash ~
+      cp -i ~/.pure_bash_prompt/.bash_aliases ~
       cp -i ~/.pure_bash_prompt/.bashrc ~
 
-      # Install gistatus to immensely speed up the rate the prompt appears. 
+      # Install gistatus to immensely speed up the rate the prompt appears.
       git clone --depth=1 https://github.com/romkatv/gitstatus.git ~/gitstatus
 
       # Don't let gitstatus be the default prompt
@@ -23,6 +22,6 @@ if [ -f ~/.pure_bash_prompt ]; then
   else
     echo "Install gitstatus before installing pure-bash-prompt"
   fi
-else 
+else
   echo "Clone the repository before installing pure-bash-prompt"
 fi
