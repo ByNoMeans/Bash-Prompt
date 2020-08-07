ef90fb1f93150aa8349322e4784bfedb9d55e114
ByNoMeans <62671086+ByNoMeans@users.noreply.github.com>
2020-08-06 19:27:44 -0500

    Update install_pure_bash_prompt.sh

--- install_pure_bash_prompt.sh
+++ install_pure_bash_prompt.sh
@@ -3,8 +3,8 @@ if [ -e ~/pure_bash_prompt ]; then
     if [ -e /c/Program\ Files/Git/ ]; then
       if [ -e /c/cygwin/etc/bash.bashrc ]; then
         cp -i ~/pure_bash_prompt/gitbash/bash.bashrc '/c/Program Files/Git/etc'
-        cp -i ~/pure_bash_prompt/gitbash/minttyrc ~
-        cp -i ~/pure_bash_prompt/gitbash/bash_profile ~
+        cp -i ~/pure_bash_prompt/gitbash/.minttyrc ~
+        cp -i ~/pure_bash_prompt/gitbash/.bash_profile ~
         cp -i ~/pure_bash_prompt/git-completion.bash ~
         cp -i ~/pure_bash_prompt/gitbash/.bash_aliases ~
         cp -i ~/pure_bash_prompt/cygwin/bash.bashrc '/c/cygwin/etc'
