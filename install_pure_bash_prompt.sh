748944eedaaae5196afadf853a80ca28a0dbf5b3
ByNoMeans <62671086+ByNoMeans@users.noreply.github.com>
2020-08-06 19:07:15 -0500

    Update install_pure_bash_prompt.sh

--- install_pure_bash_prompt.sh
+++ install_pure_bash_prompt.sh
@@ -1,14 +1,13 @@
 if [ -e ~/pure_bash_prompt ]; then
   if [ -e ~/gitstatus ]; then
     if [ -e /c/Program\ Files/Git/ ]; then
-      # Copy bash.bashrc or bash-documented.bashrc to /Git/etc/. If you prefer documentation and chose the latter, make sure to rename it to bash.bashrc
-      cp -i ~/.pure_bash_prompt/bash.bashrc '/c/Program Files/Git/etc'
-
-      # Copy .minttyrc, .bash_profile, .git-completion.bash and .bash_aliases to ~
-      cp -i ~/.pure_bash_prompt/.minttyrc ~
-      cp -i ~/.pure_bash_prompt/.bash_profile ~
-      cp -i ~/.pure_bash_prompt/.git-completion.bash ~
-      cp -i ~/.pure_bash_prompt/.bash_aliases ~
+      cp -i ~/pure_bash_prompt/bash.bashrc '/c/Program Files/Git/etc'
+      cp -i ~/pure_bash_prompt/minttyrc ~
+      cp -i ~/pure_bash_prompt/bash_profile ~
+      cp -i ~/pure_bash_prompt/git-completion.bash ~
+      cp -i ~/pure_bash_prompt/bash_aliases ~
+      echo "Install git before installing pure-bash-prompt"
+    else 
       echo "Install git before installing pure-bash-prompt"
     fi
   else 
