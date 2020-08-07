b933e142d269ed7b6f900d4e910f54af802ae802
ByNoMeans <62671086+ByNoMeans@users.noreply.github.com>
2020-08-06 19:12:36 -0500

    Update install_pure_bash_prompt.sh

--- install_pure_bash_prompt.sh
+++ install_pure_bash_prompt.sh
@@ -1,18 +1,22 @@
 if [ -e ~/pure_bash_prompt ]; then
   if [ -e ~/gitstatus ]; then
     if [ -e /c/Program\ Files/Git/ ]; then
-      cp -i ~/pure_bash_prompt/bash.bashrc '/c/Program Files/Git/etc'
-      cp -i ~/pure_bash_prompt/minttyrc ~
-      cp -i ~/pure_bash_prompt/bash_profile ~
-      cp -i ~/pure_bash_prompt/git-completion.bash ~
-      cp -i ~/pure_bash_prompt/bash_aliases ~
-      echo "Install git before installing pure-bash-prompt"
+      if [ -e /c/cygwin/etc/bash.bashrc ]; then
+        cp -i ~/pure_bash_prompt/gitbash/bash.bashrc '/c/Program Files/Git/etc'
+        cp -i ~/pure_bash_prompt/gitbash/minttyrc ~
+        cp -i ~/pure_bash_prompt/gitbash/bash_profile ~
+        cp -i ~/pure_bash_prompt/git-completion.bash ~
+        cp -i ~/pure_bash_prompt/gitbash/.bash_aliases ~
+        cp -i ~/pure_bash_prompt/cygwin/bash.bashrc '/c/cygwin/etc'
+      else
+        echo "Install cygwin before installing pure-bash-prompt, or make sure it is located in /c/cygwin"
+      fi
     else 
-      echo "Install git before installing pure-bash-prompt"
+      echo "Install git before installing pure-bash-prompt, or make sure it is located in /c/Program Files"
     fi
   else 
-    echo "Install gitstatus before installing pure-bash-prompt"
+    echo "Install gitstatus before installing pure-bash-prompt, or make sure it is located in ~"
   fi
 else
-  echo "Clone the repository before installing pure-bash-prompt"
+  echo "Clone the repository before installing pure-bash-prompt, or make sure it is located in ~"
 fi
