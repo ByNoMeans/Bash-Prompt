bea6a2838f0c17bf58a967d7d05936b7b04a2429
ByNoMeans <62671086+ByNoMeans@users.noreply.github.com>
2020-08-10 19:23:44 -0500

    Update install_pure_bash_prompt.sh

--- install_pure_bash_prompt.sh
+++ install_pure_bash_prompt.sh
@@ -1,10 +1,9 @@
-function pause(){
+function _pause(){
  read -s -n 1 -p "Press any key to continue . . ."
  echo ""
 }
 
-if [ ! -e ~/.bashrc ]; then
-  echo "test -f ~/.bash_profile && . ~/.bash_profile">~/.bashrc
+function _run() {
   if [ -e /c/cygwin/etc/bash.bashrc ]; then
     if [ -e ~/pure_bash_prompt ]; then
       if [ -e ~/gitstatus ]; then
@@ -20,25 +19,36 @@ if [ ! -e ~/.bashrc ]; then
             cp -i ~/pure_bash_prompt/cygwin/bash.bashrc '/c/cygwin/etc'
           else
 	    echo "Make sure cygwin is installed correctly. /c/cygwin/etc/nsswitch.conf does not exist."
-	    pause
+	    _pause
           fi
         else 
           echo "Install git before installing pure-bash-prompt, or make sure it is located in /c/Program Files; /c/Program\ Files/Git/ does not exist."
-          pause
+          _pause
 	fi
       else 
         echo "Install gitstatus before installing pure-bash-prompt, or make sure it is located in ~; ~/gitstatus does not exist"
-        pause
+        _pause
       fi
     else
       echo "Clone the repository before installing pure-bash-prompt, or make sure it is located in ~; ~/pure_bash_prompt does not exist"
-      pause
+      _pause
     fi
   else 
     echo "Install cygwin before installing pure-bash-prompt, or make sure it is located in /c/cygwin; /c/cygwin/etc/bash.bashrc does not exist."
-    pause
+    _pause
   fi
-else 
-  echo "A ~/.bashrc exists; will be renamed to ~/.bashrc.backup."
-  mv ~/.bashrc ~/.bashrc.backup
+}
+
+net session > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+  if [ ! -e ~/.bashrc ]; then
+    echo "test -f ~/.bash_profile && . ~/.bash_profile">~/.bashrc
+  else 
+    echo "A ~/.bashrc exists; will be renamed to ~/.bashrc.backup."
+    mv ~/.bashrc ~/.bashrc.backup
+  fi
+  _run
+else
+  echo "Run the script in an elevated bash prompt."
+  _pause
 fi
